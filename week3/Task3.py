838bbba1a9034536e2a749095d0f12872d92cea5
meruyert4 <meruyertbauyrzhanqyzy@gmail.com>
2025-01-22 23:08:00 +0500

    ass4

--- week3/Task3.py
+++ week3/Task3.py
@@ -1,12 +1,14 @@
 import numpy as np
 def iterative_method(A, B, tolerance=1e-6, max_iterations=100):
+    iter = 0
     I = np.eye(A.shape[0])
     for _ in range(max_iterations):
         E = np.dot(A, B) - I
         B = np.dot(B, I - E + np.dot(E, E))
+        iter = iter+1
         if np.linalg.norm(E) < tolerance:
             break
-    return B
+    return B, iter
 
 
 A = np.array([[1, 10, 1],
@@ -17,9 +19,10 @@ B = np.array([[0.4, 2.4, -1.4],
               [0.14, 0.14, -0.14],
               [-0.85, -3.8, 2.8]], dtype=float)
 
-refined_inverse = iterative_method(A, B)
+refined_inverse, iter = iterative_method(A, B)
 
 print("\nInitial Approximate Inverse B:")
 print(B)
-print("\nRefined Inverse of A:")
+print("\nInverse of A:")
 print(refined_inverse)
+print(iter)
