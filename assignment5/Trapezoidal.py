62547372d88c5dd63ba3c1a530fe9e566b179dc4
meruyert4 <meruyertbauyrzhanqyzy@gmail.com>
2025-01-30 11:08:19 +0500

    ass5

--- assignment5/Trapezoidal.py
+++ assignment5/Trapezoidal.py
@@ -1,11 +1,9 @@
-import numpy as np
-
 def f(x):
     return x ** 3 + x ** 2 + 3 * x + 45
 
 
 def trapezoidal_rule(f, a, b, n):
-    h = (b - a) / n  # Step size
+    h = (b - a) / n
     sum_y = f(a) + f(b)
 
     for i in range(1, n):
@@ -14,7 +12,7 @@ def trapezoidal_rule(f, a, b, n):
     return (h / 2) * sum_y
 
 
-a, b = 0, 4  # Integrate from 0 to 2
-n = 12  # Number of trapezoids
+a, b = 0, 4
+n = 12
 result = trapezoidal_rule(f, a, b, n)
 print("Approximate integral:", result)
