3328638a66a1ebaba1c023c365303c195971823b
meruyert4 <meruyertbauyrzhanqyzy@gmail.com>
2025-02-14 13:28:32 +0500

    bonus task

--- BonusProject/Task2.py
+++ BonusProject/Task2.py
@@ -3,25 +3,47 @@ import numpy as np
 time = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24])
 power = np.array([500, 480, 450, 600, 800, 950, 1000, 980, 920, 850, 700, 550, 500])
 
-# trapezoidal rule
 def trapezoidal_rule(x, y):
     n = len(x) - 1
-    integral = 0
-    for i in range(n):
-        integral += (x[i+1] - x[i]) * (y[i+1] + y[i]) / 2
-    return integral
-
-# simpsone's rule
-def simpsons_rule(x, y):
-    n = len(x) - 1
-    if n % 2 == 1:
-        raise ValueError("invalid inteval: must be even interval")
     h = (x[-1] - x[0]) / n
-    integral = y[0] + y[-1] + 4 * sum(y[1:n:2]) + 2 * sum(y[2:n-1:2])
-    return integral * h / 3
+    integral = y[0] + y[-1]
+
+    for i in range(1, n):
+        integral += 2 * y[i]
+
+    return (h / 2) * integral
+
+def simpsons_1_3_rule(x, y):
+    n = len(x) - 1
+    h = (x[-1] - x[0]) / n
+    integral = y[0] + y[-1]
+
+    for i in range(1, n, 2):  # odd
+        integral += 4 * y[i]
+    for i in range(2, n - 1, 2):  # even
+        integral += 2 * y[i]
+
+    return (h / 3) * integral
+
+
+def simpsons_3_8_rule(x, y):
+    n = len(x) - 1
+    h = (x[-1] - x[0]) / n
+    integral = y[0] + y[-1]
+
+    # Sum terms based on index
+    for i in range(1, n):
+        if i % 3 == 0:
+            integral += 2 * y[i] #every 3rd index
+        else:
+            integral += 3 * y[i]
+
+    return (3 * h / 8) * integral
 
 trapezoidal_result = trapezoidal_rule(time, power)
-simpsons_result = simpsons_rule(time, power)
+simpsons_1_3_result = simpsons_1_3_rule(time, power)
+simpsons_3_8_result = simpsons_3_8_rule(time, power)
 
 print(f"Total energy consumption using Trapezoidal Rule: {trapezoidal_result:.2f} MWh")
-print(f"Total energy consumption using Simpson's Rule: {simpsons_result:.2f} MWh")
+print(f"Total energy consumption using Simpson's 1/3 Rule: {simpsons_1_3_result:.2f} MWh")
+print(f"Total energy consumption using Simpson's 3/8 Rule: {simpsons_3_8_result:.2f} MWh")
